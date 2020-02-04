0586bb0c5d0b76ab3a9fe695241a885e3472ff0c
Quinten Van Ginderen <quintevg1@gmail.com>
2020-02-04 13:23:23 +0100

    created a bluetooth test project in c#
    adding the python files later when i get a working proof of concept

---
+++ software/c-sharp/bluetooth_test/bluetooth_test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using InTheHand.Devices.Bluetooth;
+
+namespace bluetooth_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BluetoothAdapter myadapter = new BluetoothAdapter();
+            BluetoothDevice mydevice = new BluetoothDevice();
+        }
+    }
+}
