da12628fde80eae835481b00551e77ee6cf0ac64
Quinten Van Ginderen <quintevg1@gmail.com>
2020-02-05 10:25:28 +0100

    a few edits
    bluetooth in c# should be able to scan on any computer that has bluetooth with Microsoft's drivers

---
+++ software/c-sharp/bluetooth_test/bluetooth_test/BT.cs
@@ -0,0 +1,57 @@
+﻿using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using InTheHand.Net;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bluetooth_test
+{
+    class BT
+    {
+        public string[] rows { get; set; }
+
+        public void scan()
+        {
+
+
+            BluetoothRadio.PrimaryRadio.Mode = RadioMode.Connectable;
+            BluetoothClient client = new BluetoothClient();
+            BluetoothDeviceInfo[] devices = client.DiscoverDevices();
+            BluetoothClient bluetoothClient = new BluetoothClient();
+            String authenticated;
+            String classOfDevice;
+            String connected;
+            String deviceAddress;
+            String deviceName;
+            String installedServices;
+            String lastSeen;
+            String lastUsed;
+            String remembered;
+            String rssi;
+            foreach (BluetoothDeviceInfo device in devices)
+            {
+                authenticated = device.Authenticated.ToString();
+                classOfDevice = device.ClassOfDevice.ToString();
+                connected = device.Connected.ToString();
+                deviceAddress = device.DeviceAddress.ToString();
+                deviceName = device.DeviceName.ToString();
+                installedServices = device.InstalledServices.ToString();
+                lastSeen = device.LastSeen.ToString();
+                lastUsed = device.LastUsed.ToString();
+                remembered = device.Remembered.ToString();
+                rssi = device.Rssi.ToString();
+                string[] row = new string[] { authenticated, classOfDevice, connected, deviceAddress, deviceName, installedServices, lastSeen, lastUsed, remembered, rssi };
+                Console.WriteLine(row);
+            }
+        }
+
+    }
+}
+
--- software/c-sharp/bluetooth_test/bluetooth_test/Program.cs
+++ software/c-sharp/bluetooth_test/bluetooth_test/Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
-using InTheHand.Devices.Bluetooth;
+using InTheHand.Net.Bluetooth;
+using System.Collections.Generic;
+using InTheHand.Net.Sockets;
+using InTheHand.Net;
 
 namespace bluetooth_test
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            BluetoothAdapter myadapter = new BluetoothAdapter();
-            BluetoothDevice mydevice = new BluetoothDevice();
+        static void Main(string[] args) {
+            BT test = new BT();
+            test.scan();
+            BluetoothAddress address = new BluetoothAddress(0XB827EBE12D86); //bluetoothaddress of the device
+            Guid guid = new Guid();
+            BluetoothEndPoint myendpoint = new BluetoothEndPoint(address, new Guid()); //address of the rasperry pi
+            BluetoothClient client = new BluetoothClient(); //this computer
+            client.Connect(myendpoint);
+            while (client.Connected)
+            {
+                Console.WriteLine(client.GetStream());
+            }
+            
         }
+
+
     }
 }
+
