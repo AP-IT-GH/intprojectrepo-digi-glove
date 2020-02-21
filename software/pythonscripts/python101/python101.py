d7b25e14dc36029fbc650b94d16877b8d4eaca26
Quinten Van Ginderen <quintevg1@gmail.com>
2020-02-21 16:24:50 +0100

    bluetooth save
    simple save of progress

---
+++ software/pythonscripts/python101/python101.py
@@ -0,0 +1,5 @@
+#how to program in python
+#first a word, python is a language that is run by an interperator, so the code is executed as the interperator reads the lines
+#which would mean that it's executed as a script, though python can also create objects ans classes.
+#every python script can be included in any script as long as it's located on the same drive.
+print("test")
