4bb22a7fed3ba418ab8fd7af7d6fbfcd9158a1a8
Zeno <s102215@ap.be>
2020-03-10 15:31:19 +0100

    Updated layout of configuration tab

--- software/Digi-Glove-Application/Configurations.Designer.cs
+++ software/Digi-Glove-Application/Configurations.Designer.cs
@@ -29,34 +29,36 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Configurations));
-            this.panel_configuration = new System.Windows.Forms.Panel();
+            this.panel_calibration = new System.Windows.Forms.Panel();
             this.panel_text = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button_title_calibration = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel_control = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel_macro = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.panel_configuration.SuspendLayout();
+            this.panel_calibration.SuspendLayout();
             this.panel_text.SuspendLayout();
-            this.panel1.SuspendLayout();
+            this.panel_control.SuspendLayout();
             this.panel3.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.panel_macro.SuspendLayout();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel_configuration
+            // panel_calibration
             // 
-            this.panel_configuration.Controls.Add(this.panel_text);
-            this.panel_configuration.Controls.Add(this.button_title_calibration);
-            this.panel_configuration.Location = new System.Drawing.Point(34, 29);
-            this.panel_configuration.Name = "panel_configuration";
-            this.panel_configuration.Size = new System.Drawing.Size(653, 177);
-            this.panel_configuration.TabIndex = 1;
+            this.panel_calibration.Controls.Add(this.panel_text);
+            this.panel_calibration.Controls.Add(this.button_title_calibration);
+            this.panel_calibration.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_calibration.Location = new System.Drawing.Point(0, 0);
+            this.panel_calibration.Margin = new System.Windows.Forms.Padding(0);
+            this.panel_calibration.Name = "panel_calibration";
+            this.panel_calibration.Size = new System.Drawing.Size(706, 177);
+            this.panel_calibration.TabIndex = 1;
             // 
             // panel_text
             // 
@@ -65,7 +67,7 @@
             this.panel_text.Location = new System.Drawing.Point(0, 34);
             this.panel_text.Name = "panel_text";
             this.panel_text.Padding = new System.Windows.Forms.Padding(20);
-            this.panel_text.Size = new System.Drawing.Size(653, 143);
+            this.panel_text.Size = new System.Drawing.Size(706, 143);
             this.panel_text.TabIndex = 3;
             // 
             // textBox1
@@ -78,9 +80,10 @@
             this.textBox1.Enabled = false;
             this.textBox1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(20, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(613, 103);
+            this.textBox1.Size = new System.Drawing.Size(666, 103);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
@@ -93,22 +96,25 @@
             this.button_title_calibration.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button_title_calibration.Image = ((System.Drawing.Image)(resources.GetObject("button_title_calibration.Image")));
             this.button_title_calibration.Location = new System.Drawing.Point(0, 0);
+            this.button_title_calibration.Margin = new System.Windows.Forms.Padding(0);
             this.button_title_calibration.Name = "button_title_calibration";
-            this.button_title_calibration.Size = new System.Drawing.Size(653, 34);
+            this.button_title_calibration.Size = new System.Drawing.Size(706, 34);
             this.button_title_calibration.TabIndex = 0;
             this.button_title_calibration.Text = "Calibration";
             this.button_title_calibration.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button_title_calibration.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.button_title_calibration.UseVisualStyleBackColor = true;
             // 
-            // panel1
+            // panel_control
             // 
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(34, 212);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(653, 177);
-            this.panel1.TabIndex = 2;
+            this.panel_control.Controls.Add(this.panel3);
+            this.panel_control.Controls.Add(this.button1);
+            this.panel_control.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_control.Location = new System.Drawing.Point(0, 177);
+            this.panel_control.Margin = new System.Windows.Forms.Padding(0);
+            this.panel_control.Name = "panel_control";
+            this.panel_control.Size = new System.Drawing.Size(706, 177);
+            this.panel_control.TabIndex = 2;
             // 
             // panel3
             // 
@@ -117,7 +123,7 @@
             this.panel3.Location = new System.Drawing.Point(0, 34);
             this.panel3.Name = "panel3";
             this.panel3.Padding = new System.Windows.Forms.Padding(20);
-            this.panel3.Size = new System.Drawing.Size(653, 143);
+            this.panel3.Size = new System.Drawing.Size(706, 143);
             this.panel3.TabIndex = 3;
             // 
             // textBox2
@@ -130,9 +136,10 @@
             this.textBox2.Enabled = false;
             this.textBox2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(20, 20);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(0);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(613, 103);
+            this.textBox2.Size = new System.Drawing.Size(666, 103);
             this.textBox2.TabIndex = 1;
             this.textBox2.Text = resources.GetString("textBox2.Text");
             // 
@@ -145,22 +152,25 @@
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(653, 34);
+            this.button1.Size = new System.Drawing.Size(706, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "Controls";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // panel2
+            // panel_macro
             // 
-            this.panel2.Controls.Add(this.panel4);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Location = new System.Drawing.Point(34, 395);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(653, 177);
-            this.panel2.TabIndex = 2;
+            this.panel_macro.Controls.Add(this.panel4);
+            this.panel_macro.Controls.Add(this.button2);
+            this.panel_macro.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_macro.Location = new System.Drawing.Point(0, 354);
+            this.panel_macro.Margin = new System.Windows.Forms.Padding(0);
+            this.panel_macro.Name = "panel_macro";
+            this.panel_macro.Size = new System.Drawing.Size(706, 177);
+            this.panel_macro.TabIndex = 2;
             // 
             // panel4
             // 
@@ -169,7 +179,7 @@
             this.panel4.Location = new System.Drawing.Point(0, 34);
             this.panel4.Name = "panel4";
             this.panel4.Padding = new System.Windows.Forms.Padding(20);
-            this.panel4.Size = new System.Drawing.Size(653, 143);
+            this.panel4.Size = new System.Drawing.Size(706, 143);
             this.panel4.TabIndex = 3;
             // 
             // textBox3
@@ -182,9 +192,10 @@
             this.textBox3.Enabled = false;
             this.textBox3.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.Location = new System.Drawing.Point(20, 20);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(0);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(613, 103);
+            this.textBox3.Size = new System.Drawing.Size(666, 103);
             this.textBox3.TabIndex = 1;
             this.textBox3.Text = resources.GetString("textBox3.Text");
             // 
@@ -197,8 +208,9 @@
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(653, 34);
+            this.button2.Size = new System.Drawing.Size(706, 34);
             this.button2.TabIndex = 0;
             this.button2.Text = "Macro\'s";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -211,18 +223,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(237)))), ((int)(((byte)(245)))));
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel_configuration);
+            this.Controls.Add(this.panel_macro);
+            this.Controls.Add(this.panel_control);
+            this.Controls.Add(this.panel_calibration);
+            this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "Configurations";
             this.Size = new System.Drawing.Size(706, 483);
-            this.panel_configuration.ResumeLayout(false);
+            this.panel_calibration.ResumeLayout(false);
             this.panel_text.ResumeLayout(false);
             this.panel_text.PerformLayout();
-            this.panel1.ResumeLayout(false);
+            this.panel_control.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            this.panel2.ResumeLayout(false);
+            this.panel_macro.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.ResumeLayout(false);
@@ -231,11 +244,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel_configuration;
+        private System.Windows.Forms.Panel panel_calibration;
         private System.Windows.Forms.Button button_title_calibration;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel_control;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel_macro;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel_text;
         private System.Windows.Forms.TextBox textBox1;
