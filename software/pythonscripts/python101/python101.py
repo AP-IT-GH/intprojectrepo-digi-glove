339799396c181213eebbbadf087ed1479e82f9db
Zeno <s102215@ap.be>
2020-03-10 17:12:49 +0100

    Revert "bluetooth fixed"
    This reverts commit 5e4181d3c1cb37ec243ad52da1d6a634fd44efd2.

--- software/pythonscripts/python101/python101.py
+++ software/pythonscripts/python101/python101.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import serial
 ser = serial.Serial(r"\\.\COM15")
 ser1 = serial.Serial(r"\\.\COM13")
@@ -5,3 +6,17 @@ print("sending data continiously to bluetooth devices press ctrl + c to stop")
 while True:
     ser.write(0x00)
     ser1.write(0x01)
+=======
+
+import serial
+#ser = serial.Serial(r"\\.\COM15")
+#ser1 = serial.Serial(r"\\.\COM13")
+#ser = serial.Serial("COM15", 9600, 8)
+ser1 = serial.Serial("COM13", 9600, 8)
+print("sending data continiously to bluetooth devices press ctrl + c to stop")
+while True:
+    #ser.write(0XFF)
+    ser1.write(0x01)
+    #print(ser.read(2))
+    print(ser1.read(2))
+>>>>>>> parent of 5e4181d... bluetooth fixed
