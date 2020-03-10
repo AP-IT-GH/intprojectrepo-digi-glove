cd428f6d1337f909ae9c2ce5e643fcd68ba64e9e
Zeno <s102215@ap.be>
2020-03-10 17:13:55 +0100

    Revert "bluetooth works"
    This reverts commit e378234a3672f72b054edf9deb376d889557675b.

--- software/pythonscripts/python101/python101.py
+++ software/pythonscripts/python101/python101.py
@@ -9,13 +9,11 @@ while True:
 =======
 
 import serial
-#ser = serial.Serial(r"\\.\COM15")
-#ser1 = serial.Serial(r"\\.\COM13")
-#ser = serial.Serial("COM15", 9600, 8)
-ser1 = serial.Serial("COM13", 9600, 8)
+ser = serial.Serial(r"\\.\COM15")
+ser1 = serial.Serial(r"\\.\COM13")
 print("sending data continiously to bluetooth devices press ctrl + c to stop")
 while True:
-    #ser.write(0XFF)
+    ser.write(0x00)
     ser1.write(0x01)
     #print(ser.read(2))
     print(ser1.read(2))
