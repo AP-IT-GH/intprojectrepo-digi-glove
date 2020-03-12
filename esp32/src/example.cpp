ce3cdc74c327b70bd24b7c50c5688d9411c4ca92
Nasse <t8kana00@students.oamk.fi>
2020-03-12 02:47:29 +0200

    Partially ported arduino example for DMP 6.12, ypr now works

--- esp32/src/example.cpp
+++ esp32/src/example.cpp
@@ -11,18 +11,32 @@
 #include <freertos/task.h>
 #include "MPU6050.h"
 #include "MPU6050_6Axis_MotionApps_V6_12.h"
+//#include "MPU6050_6Axis_MotionApps20.h"
 #include "sdkconfig.h"
 
 #define PIN_SDA 22
 #define PIN_CLK 21
-
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-uint16_t packetSize = 42;    // expected DMP packet size (default is 42 bytes)
+#define OUTPUT_READABLE_YAWPITCHROLL
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 gy;         // [x, y, z]            gyro sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
@@ -56,40 +70,90 @@ void task_display(void*){
     mpu.setZAccelOffset(9726);
 
 	mpu.setDMPEnabled(true);
-	
+	dmpReady = true;
 
 	while(1){
-	    mpuIntStatus = mpu.getIntStatus();
-		// get current FIFO count
-		fifoCount = mpu.getFIFOCount();
-		
-		
-	    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-	        // reset so we can continue cleanly
-	        mpu.resetFIFO();
+				// if programming failed, don't try to do anything
+		if (!dmpReady) return;
+		// read a packet from FIFO
+		if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
 
-	    // otherwise, check for DMP data ready interrupt frequently)
-	    } else if (mpuIntStatus & 0x02) {
-	        // wait for correct available data length, should be a VERY short wait
-	        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+		#ifdef OUTPUT_READABLE_QUATERNION
+			// display quaternion values in easy matrix form: w x y z
+			mpu.dmpGetQuaternion(&q, fifoBuffer);
+			printf"quat\t");
+			printfq.w);
+			printf"\t");
+			printfq.x);
+			printf"\t");
+			printfq.y);
+			printf"\t");
+			Serial.println(q.z);
+		#endif
 
-	        // read a packet from FIFO
+		#ifdef OUTPUT_READABLE_EULER
+			// display Euler angles in degrees
+			mpu.dmpGetQuaternion(&q, fifoBuffer);
+			mpu.dmpGetEuler(euler, &q);
+			printf"euler\t");
+			printfeuler[0] * 180 / M_PI);
+			printf"\t");
+			printfeuler[1] * 180 / M_PI);
+			printf"\t");
+			Serial.println(euler[2] * 180 / M_PI);
+		#endif
 
-	        mpu.getFIFOBytes(fifoBuffer, packetSize);
-	 		mpu.dmpGetQuaternion(&q, fifoBuffer);
+		#ifdef OUTPUT_READABLE_YAWPITCHROLL
+			// display Euler angles in degrees
+			mpu.dmpGetQuaternion(&q, fifoBuffer);
 			mpu.dmpGetGravity(&gravity, &q);
 			mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-			printf("YAW: %3.1f, ", ypr[0] * 180/PI);
-			printf("PITCH: %3.1f, ", ypr[1] * 180/PI);
-		
-			printf("ROLL: %3.1f \n", ypr[2] * 180/PI);
-	    }
+			printf("%f \t %f \t %f \r\n", ypr[0] * 180/PI,ypr[1] * 180/PI,ypr[2] * 180/PI);
+		#endif
 
-	    //Best result is to match with DMP refresh rate
-	    // Its last value in components/MPU6050/MPU6050_6Axis_MotionApps20.h file line 310
-	    // Now its 0x13, which means DMP is refreshed with 10Hz rate
-		//vTaskDelay(50/portTICK_PERIOD_MS);
+		#ifdef OUTPUT_READABLE_REALACCEL
+			// display real acceleration, adjusted to remove gravity
+			mpu.dmpGetQuaternion(&q, fifoBuffer);
+			mpu.dmpGetAccel(&aa, fifoBuffer);
+			mpu.dmpGetGravity(&gravity, &q);
+			mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+			printf"areal\t");
+			printfaaReal.x);
+			printf"\t");
+			printfaaReal.y);
+			printf"\t");
+			Serial.println(aaReal.z);
+		#endif
+
+		#ifdef OUTPUT_READABLE_WORLDACCEL
+			// display initial world-frame acceleration, adjusted to remove gravity
+			// and rotated based on known orientation from quaternion
+			mpu.dmpGetQuaternion(&q, fifoBuffer);
+			mpu.dmpGetAccel(&aa, fifoBuffer);
+			mpu.dmpGetGravity(&gravity, &q);
+			mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+			mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+			printf"aworld\t");
+			printfaaWorld.x);
+			printf"\t");
+			printfaaWorld.y);
+			printf"\t");
+			Serial.println(aaWorld.z);
+		#endif
+
+		#ifdef OUTPUT_TEAPOT
+			// display quaternion values in InvenSense Teapot demo format:
+			teapotPacket[2] = fifoBuffer[0];
+			teapotPacket[3] = fifoBuffer[1];
+			teapotPacket[4] = fifoBuffer[4];
+			teapotPacket[5] = fifoBuffer[5];
+			teapotPacket[6] = fifoBuffer[8];
+			teapotPacket[7] = fifoBuffer[9];
+			teapotPacket[8] = fifoBuffer[12];
+			teapotPacket[9] = fifoBuffer[13];
+			Serial.write(teapotPacket, 14);
+			teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+		#endif
+		}
 	}
-
-	vTaskDelete(NULL);
 }
