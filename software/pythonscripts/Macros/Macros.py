3b277424819014ef5655a3caa11c987034525d55
Stef Martens <stef.m@hotmail.be>
2020-03-22 00:24:22 +0100

    Beginning of socket code

--- software/pythonscripts/Macros/Macros.py
+++ software/pythonscripts/Macros/Macros.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3.8
 from ctypes import windll, Structure, c_long, byref
 import pyautogui
+import socket
+
 
 
 class POINT(Structure):
@@ -49,35 +51,47 @@ accelerationXaxis = 0
 accelerationYaxis = 0
 accelerationZaxis = 0
 
-#the indexfinger is bend when the value of the flex resistor (2 flex sensors on each finger) is larger than 200 for each
-if(flexFinger1>=200 and flexFinger2>=200):
-    thumb=True
-else:
-    thumb=False
 
-if(flexFinger3>= 200 and flexFinger4>=200):
-    indexFinger=True
-else:
-    indexFinger=False
 
-if(flexFinger5>=200 and flexFinger6>=200):
-    middleFinger=True
-else:
-    middleFinger=False
+#socket
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((socket.gethostname(),1234))
+s.listen(5)
 
-if(flexFinger7>=200 and flexFinger8>=200):
-    ringFinger=True
-else:
-    ringFinger=False
+while True:
+    clientsocket,address=s.accept()
+    print(f"Connection from {address} has been established!")
+    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
 
-if(flexFinger9>=200 and flexFinger10>=200):
-    littleFinger=True
-else:
-    littleFinger=False
+    #the indexfinger is bend when the value of the flex resistor (2 flex sensors on each finger) is larger than 200 for each
+    if(flexFinger1>=200 and flexFinger2>=200):
+        thumb=True
+    else:
+        thumb=False
 
-#the CallMacro function gets the value of each finger
-#in the MacroClass the corresponding macro gets activated
-if(indexFinger): PrintScreen();
+    if(flexFinger3>= 200 and flexFinger4>=200):
+        indexFinger=True
+    else:
+        indexFinger=False
+
+    if(flexFinger5>=200 and flexFinger6>=200):
+        middleFinger=True
+    else:
+        middleFinger=False
+
+    if(flexFinger7>=200 and flexFinger8>=200):
+        ringFinger=True
+    else:
+        ringFinger=False
+
+    if(flexFinger9>=200 and flexFinger10>=200):
+        littleFinger=True
+    else:
+       littleFinger=False
+
+    #the CallMacro function gets the value of each finger
+    #in the MacroClass the corresponding macro gets activated
+    if(indexFinger): PrintScreen();
 
 def RightMouseClick():
     pt = POINT()
@@ -121,5 +135,3 @@ def Cut():
 
 def Bold():
     pyautogui.hotkey('ctrl','b')
-
-CloseCommandPrompt()
