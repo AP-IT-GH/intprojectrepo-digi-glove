917aea92b8453e85946f49b9d314283991805533
Quinten Van Ginderen <quintevg1@gmail.com>
2020-03-23 14:10:50 +0100

    Bluetooth data to dictionary conversion started

--- software/pythonscripts/python101/python101.py
+++ software/pythonscripts/python101/python101.py
@@ -2,7 +2,7 @@ import Macros
 import serial
 import time
 
-port = "COM13"
+port = "COM15"
 
 ser1 = serial.Serial(port, 9600, 8)
 print("sending data continiously to bluetooth devices press ctrl + c to stop")
@@ -11,8 +11,14 @@ data = {"value1" : 101 , "value2" : 101}
 def update(): #to edit in release
        #ser1.write(0x01)
        if(ser1.inWaiting() >= 64):
-            print(ser1.read(64))
-       print("/")
+            data = ser1.read(64)
+            #data.split("x")
+            str(data)
+            print(data)
+            print(data[0: 4])
+            print("/")
+            time.sleep(10) #wait for 10 seconds
+            ser1.reset_input_buffer() #clear the buffer
        #data["value1"] = ser1.read()
        #time.sleep(0.005) #decide the frequency of pulls esp data is presumed live
 #endupdate
