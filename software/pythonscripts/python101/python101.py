b32cf4e7ff602dc0fef08c406bfdb551bf1e032c
Quinten Van Ginderen <quintevg1@gmail.com>
2020-04-18 11:47:15 +0200

    Recreated the python-dev branch and project .
    as i accidentally removed the python project file from the program, i recreated it now. with the code as up to date as possible

--- software/pythonscripts/python101/python101.py
+++ software/pythonscripts/python101/python101.py
@@ -1,10 +1,6 @@
+import Macros
 import serial
-ser = serial.Serial(r"\\.\COM15")
-ser1 = serial.Serial(r"\\.\COM13")
-print("sending data continiously to bluetooth devices press ctrl + c to stop")
-while True:
-    ser.write(0x00)
-    ser1.write(0x01)
+import time
 
 
 port = "COM15"
