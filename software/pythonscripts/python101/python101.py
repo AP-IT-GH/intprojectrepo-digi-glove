000bea2da59f65d90cf0e51bdffd86046bd38d7b
Quinten Van Ginderen <quintevg1@gmail.com>
2020-04-25 16:07:09 +0200

    fingerstatus added

--- software/pythonscripts/python101/python101.py
+++ software/pythonscripts/python101/python101.py
@@ -26,8 +26,7 @@ def update(): #to edit in release
        try:
             if(ser1.inWaiting() >= 64):
                 data_seq = ser1.read(64) #read the buffer as soon as it reached 64 bytes aka a full sequence has entered1
-                print(data_seq[38:50])
-                #print(data_seq)
+                print(data["IndexF_0"], data["MiddleF_0"], data["RingF_0"], data["LittleF_0"], data["Thumb_0"])
                 data["reserved_0"] = int(data_seq[0])
                 data["reserved_1"] = int(data_seq[1])
                 data["reserved_2"] = int(data_seq[2])
@@ -126,6 +125,7 @@ def update(): #to edit in release
                 #data["IndexF_0"] = 205
                 #data["IndexF_tip"] = 205
                 #data["Thumb_0"] = 205
+                
 
        except:
         #print("no connection all data is 0")
